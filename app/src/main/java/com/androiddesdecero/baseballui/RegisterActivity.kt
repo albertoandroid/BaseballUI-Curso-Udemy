efe0a81357ad565cc378b6a926f4ce1fba151c7e
Alberto <info@androiddesdecero.com>
2018-03-29 13:01:04 +0200

    Register Activity

---
+++ app/src/main/java/com/androiddesdecero/baseballui/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.androiddesdecero.baseballui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
