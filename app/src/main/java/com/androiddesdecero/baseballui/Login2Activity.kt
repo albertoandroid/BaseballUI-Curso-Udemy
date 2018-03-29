f1ac20ad1b733ceedce9fcef3bf6571c6b976dff
Alberto <info@androiddesdecero.com>
2018-03-29 12:27:08 +0200

    Login Activity

---
+++ app/src/main/java/com/androiddesdecero/baseballui/Login2Activity.kt
@@ -0,0 +1,12 @@
+package com.androiddesdecero.baseballui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Login2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login2)
+    }
+}
