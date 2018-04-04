e0df111add06b6364f675c996512a66dd5cd2d3a
Alberto <info@androiddesdecero.com>
2018-04-04 13:23:35 +0200

    animaciones Transiciones

--- app/src/main/java/com/androiddesdecero/baseballui/HomeAnimationActivity.kt
+++ app/src/main/java/com/androiddesdecero/baseballui/HomeAnimationActivity.kt
@@ -1,8 +1,12 @@
 package com.androiddesdecero.baseballui
 
+import android.app.ActivityOptions
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.util.Pair
+
 import kotlinx.android.synthetic.main.activity_home_animation.*
 
 class HomeAnimationActivity : AppCompatActivity() {
@@ -20,5 +24,18 @@ class HomeAnimationActivity : AppCompatActivity() {
             startActivity(Intent(this@HomeAnimationActivity, FadeActivity::class.java))
             overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
         })
+
+        homeAnimationActivityTvTransition.setOnClickListener({
+            var pair: Pair<View, String> = Pair<View,String>(homeAnimationActivityIv, "ivTransition")
+            var pair2: Pair<View, String> = Pair<View,String>(homeAnimationActivityTvTransition, "tvTransition")
+
+            var intent = Intent(this@HomeAnimationActivity, TransitionActivity::class.java)
+            var optiones = ActivityOptions.makeSceneTransitionAnimation(this, pair, pair2)
+
+            startActivity(intent, optiones.toBundle())
+
+
+        })
+
     }
 }
